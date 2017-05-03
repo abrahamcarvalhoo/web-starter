9684a802238e77ea6c99e8347a2ddd13327044d6
Abraham Carvalho <abraham.carvalhoo@gmail.com>
2017-05-03 17:04:38 -0300

    added jquery-validation

--- source/assets/javascripts/requirements.js
+++ source/assets/javascripts/requirements.js
@@ -1,1 +1,4 @@
 //=include jquery/dist/jquery.js
+//=include jquery-validation/dist/jquery.validate.js
+//=include jquery-validation/dist/additional-methods.js
+//=include jquery-validation/src/localization/messages_pt_BR.js
