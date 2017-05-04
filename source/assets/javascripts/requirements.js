e1524fbe6b48f80d3f2dc6eeaa8f661c89e996fc
Abraham Carvalho <abraham.carvalhoo@gmail.com>
2017-05-04 16:38:57 -0300

    added jquery-mask-plugin

--- source/assets/javascripts/requirements.js
+++ source/assets/javascripts/requirements.js
@@ -2,3 +2,4 @@
 //=include jquery-validation/dist/jquery.validate.js
 //=include jquery-validation/dist/additional-methods.js
 //=include jquery-validation/src/localization/messages_pt_BR.js
+//=include jquery-mask-plugin/dist/jquery.mask.js
